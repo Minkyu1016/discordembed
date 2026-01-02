1946e657d2d894f57d2d5cf8f60a6e19aff7f8a6
Minkyu1016 <minkyu4635@gmail.com>
2026-01-02 10:47:47 +0900

    Update create.js

--- api/create.js
+++ api/create.js
@@ -1,21 +1,14 @@
-import crypto from "crypto";
-
-const store = global.store ?? (global.store = new Map());
-
 export default function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).end();
   }
 
-  let id;
-  do {
-    id = crypto.randomBytes(3).toString("base64url");
-  } while (store.has(id)); // 중복 방지
+  const id = Math.random().toString(36).slice(2, 6);
 
-  store.set(id, req.body);
+  global.embeds ||= {};
+  global.embeds[id] = req.body;
 
   res.json({
     url: `${req.headers.origin}/api/e/${id}`
   });
 }
-
